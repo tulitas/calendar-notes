f7937d08c7208302dfa39b5f7fe48f1154ad5a38
SergejK <tulitas1@gmail.com>
2019-07-24 23:59:45 +0300

    oj

---
+++ src/main/java/name/alexkosarev/tutorials/controllers/CalendarController.java
@@ -0,0 +1,14 @@
+package name.alexkosarev.tutorials.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class CalendarController {
+    @RequestMapping("/")
+    String home() {
+        return "hi";
+    }
+}
