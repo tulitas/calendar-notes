02f2d1dbaf4d52570e8156b34c9fdfe97906e2be
SergejK <tulitas1@gmail.com>
2019-07-27 00:09:57 +0300

    ne otobrazart god

--- src/main/java/com/controllers/SiteController.java
+++ src/main/java/com/controllers/SiteController.java
@@ -2,6 +2,7 @@ package com.controllers;
 
 import java.util.Collections;
 import java.util.Date;
+import java.util.Map;
 
 import com.entities.Review;
 import com.repositories.ReviewRepository;
@@ -22,22 +23,12 @@ public class SiteController {
     public ModelAndView index() {
         return new ModelAndView("site/index", Collections.singletonMap("reviews", reviewRepository.findAll()));
     }
-    @RequestMapping(value = {"/year2019"})
-    public String viewYear2019()  {
+    @RequestMapping("/year2019")
+    public String next() {
+
         return "year2019";
     }
 
-    //    @RequestMapping
-//    public ModelAndView year2019() {
-//        return new ModelAndView("year2019", Collections.singletonMap("reviews", reviewRepository.findAll()));
-//    }
-//    @RestController
-//    public class YearsController {
-//        @RequestMapping("/")
-//        public String year2019() {
-//            return "hello";
-//        }
-//    }
 
     @RequestMapping(method = RequestMethod.POST)
     public String createReview(@RequestParam String reviewText) {
