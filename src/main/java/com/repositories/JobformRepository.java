240b450c0a0248217dff4a6c02176dd8d2cb5397
SergejK <tulitas1@gmail.com>
2019-08-26 22:55:08 +0300

    delete

--- src/main/java/com/repositories/JobformRepository.java
+++ src/main/java/com/repositories/JobformRepository.java
@@ -4,9 +4,8 @@ package com.repositories;
 
 import com.entities.JobForm;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JobformRepository extends JpaRepository<JobForm, Integer> {
+interface JobformRepository extends JpaRepository<JobForm, Integer> {
 }
