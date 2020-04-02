cc6bdbc2b6d60a42714cbc017eae0b9143d249ab
SergejK <tulitas1@gmail.com>
2020-04-02 15:39:51 +0300

    sdelal taimer dlja sms

--- src/main/java/com/SmsMaker/SmsConnect.java
+++ src/main/java/com/SmsMaker/SmsConnect.java
@@ -3,6 +3,7 @@ package com.SmsMaker;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -11,6 +12,7 @@ import java.util.TimerTask;
 public class SmsConnect extends TimerTask {
     private static ParallelScanner in;
     private static PrintStream out;
+    private static Socket socket;
     private MakeSms makeSms = new MakeSms();
 
     @Override
@@ -34,24 +36,28 @@ public class SmsConnect extends TimerTask {
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
-                readFromDevice();
+                try {
+                    readFromDevice();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
 
         };
         Timer timer = new Timer(true);
-        timer.scheduleAtFixedRate(timerTask, 0, 10 * 300);
+        timer.scheduleAtFixedRate(timerTask, 0, 10 * 260);
         System.out.println("na4al vipolnenie");
         try {
-            Thread.sleep(5000);
+            Thread.sleep(5480);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-   public void Savienojums() {
+    public void Savienojums() {
         try {
-            Socket socket = new Socket("192.168.6.80", 5038);
+            socket = new Socket("192.168.6.80", 5038);
             out = new PrintStream(socket.getOutputStream());
             in = new ParallelScanner(new Scanner(socket.getInputStream()));
             in.start();
@@ -68,18 +74,24 @@ public class SmsConnect extends TimerTask {
     }
 
 
-    private void readFromDevice() {
+    private void readFromDevice() throws IOException {
+
+        try {
 
 
-        String line = in.nextLine();
-        while (in.hasNext()) {
-            if (line.equals("Message: Authentication accepted")) {
-                System.out.println("Action: smscommand\r\ncommand: gsm send sms 2 " + "29158366"
-                        + " \"12345 status\" " + "01.02.2020" + "\r\n\r\n");
-                return;
+            String line = in.nextLine();
+            while (in.hasNext()) {
+                if (line.equals("Message: Authentication accepted")) {
+                    System.out.println(makeSms.getRememberSms());
+                    socket.close();
+                    System.out.println("soedinenie zakrito");
+//                return;
+                }
             }
-
+        } catch
+        (NoSuchElementException e) {
+            e.printStackTrace();
         }
-    }
 
+    }
 }
